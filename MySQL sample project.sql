35900088115d0d313ef617bffc5390fcf01c57c5
Manikandan <111624403+Manikandan22052001@users.noreply.github.com>
2022-08-20 09:59:10 +0530

    Add files via upload

---
+++ MySQL sample project.sql
@@ -0,0 +1,10 @@
+use student;
+
+select * from STUDENT;
+select name from stuDENT where TOTAL_PERCENTAGE ;
+
+select name, TOTAL_1, TOTAL_2, percentage_1, percentage_1, TOTAL_PERCENTAGE from STUDENT where TOTAL_PERCENTAGE>=85;
+select * from STUDENT where TOTAL_PERCENTAGE>=85;
+
+ALTER TABLE SUDENT rename TO STUDENT;
+
